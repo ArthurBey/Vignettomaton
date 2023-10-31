2c274e081ff3cf4309d59cda4d45c5095677bac9
arthur <arthur.bachirov@gmail.com>
2023-10-31 12:18:18 +0100

    Procedural - first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Vignettomaton</title>
+</head>
+<body>
+<h1>Vignettomaton</h1>
+<p>Provide the 3 format files with the "EU" variation</p>
+<form method="post" action="process.php">
+    <input type="submit" name="run_script" value="Run Script">
+</form>
+</body>
+</html>
---
+++ process.php
@@ -0,0 +1,18 @@
+<?php
+if (isset($_POST['run_script'])) {
+    $sourceDir = ""; // BLANK
+    $destinationDir = ""; // BLANK
+    $prefix = $sourceDir
+
+    $countryCodes = ["BE", "DE", "BG", "SE", "DK", "NL", "ISL", "GF", "UK", "NO"];
+
+    foreach ($countryCodes as $code) {
+        $fileExtensions = ["lg", "sm", "tn"];
+
+        $commonPrefix = substr($sourceFile,)
+
+        foreach ($fileExtensions as $ext) {
+            $sourceFile = $sourceDir . ""
+        }
+    }
+}
